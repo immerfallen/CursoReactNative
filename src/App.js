094ef3adba6afbb34957aae41fe7db121e254005
Maro de Melo <marodemelo10@gmail.com>
2021-03-15 20:24:40 -0300

    Primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {View, Text} from 'react-native'
+import Primeiro from './components/Primeiro'
+
+export default () => (
+    <View>
+        <Primeiro> </Primeiro> 
+        <Text>{1+1}</Text>
+    </View> 
+) // Pode-se utilizar da seguinte forma o componente <Primeiro /> O que está dentro das chaves é intepretado com um código JS 
+
+// COMPONENTES BASEADOS EM FUNÇÕES SÃO CHAMADOS DE COMPONENTES FUNCIONAIS
 
 /* function App(){
     return <Text>Primeiro Componente</Text>}
@@ -18,4 +28,4 @@ export default App */
     return <Text>Primeiro Componente</Text> TAMBÉM É POSSÍVEL CRIAR OM COMPONENTE COMO UMA ARROW FUNCTION
 } */
 
- export default () => <Text>Primeiro componente!!</Text>    
+ /* export default () => <Text>Primeiro componente!!</Text>    // TAMBÉM É POSSÍVEL CRIAR O COMPONENTE DE MANEIRA MÍNIMA EM UMA LINHA */
