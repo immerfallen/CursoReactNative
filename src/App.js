3b6d60837e88aaf61b49e9f1bd992bb5c4ca9f7a
Maro de Melo <marodemelo10@gmail.com>
2021-03-15 19:48:25 -0300

    criado primeiro componente

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+/* function App(){
+    return <Text>Primeiro Componente</Text>}
+
+export default App */
+
+/* const App = function(){
+    return <Text>Primeiro Componente</Text>  TAMBÉM É POSSÍVEL UTILIZAR FUNÇÃO ANÔNIMA
+} */
+
+/* export deafult function(){
+    return <Text>Primeiro Componente</Text>  A FUNÇÃO REPRESENTA UM COMPONENTE FUNCIONAL
+} */
+
+ /* export deafult () => {
+    return <Text>Primeiro Componente</Text> TAMBÉM É POSSÍVEL CRIAR OM COMPONENTE COMO UMA ARROW FUNCTION
+} */
+
+ export default () => <Text>Primeiro componente!!</Text>    
