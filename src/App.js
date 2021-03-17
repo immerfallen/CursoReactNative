2816235b558db01edf2880fad5ec417547db8550
Maro de Melo <marodemelo10@gmail.com>
2021-03-17 18:25:02 -0300

    React Fragment

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 // import Primeiro from './components/Primeiro'
 // import X, {Comp1, Comp2 } from './components/MultiComponents' //Esse X é o componente oficial (default) exportado de MultiComponents
-import MinMax from './components/MinMax'
-import Aleatorio from './components/Aleatorio'
+// import MinMax from './components/MinMax'
+// import Aleatorio from './components/Aleatorio'
+import Principal from './components/Titulo'
 
 export default () => (
     <View style={style.App}>
@@ -14,7 +15,9 @@ export default () => (
         <Comp2></Comp2>    */}
         {/* <MinMax  min="3" max="20"/> 
         <MinMax  min="1" max="90"/>  */}
-        <Aleatorio min={100} max={200}></Aleatorio>
+        {/* <Aleatorio min={100} max={200}></Aleatorio> */}
+        <Principal principal="Cadastro de Produto" secundario="Entre com os dados para cadastrar o produto:"></Principal>
+        
     </View> 
 ) // Pode-se utilizar da seguinte forma o componente <Primeiro /> O que está dentro das chaves é intepretado com um código JS 
 
